ea753e84da11df26da9d952a85eeeb4b48270e85
KolesnikAntony <50579516+KolesnikAntony@users.noreply.github.com>
2021-01-05 20:28:33 +0200

    change BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -3,21 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import store from "./redux/redux-store";
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 
 
-    ReactDOM.render(
-        <React.StrictMode>
-            <BrowserRouter>
-                <Provider store={store}>
-                <App />
-                </Provider>
-            </BrowserRouter>
-        </React.StrictMode>,
-        document.getElementById('root')
-    );
+ReactDOM.render( <
+    React.StrictMode >
+    <
+    HashRouter >
+    <
+    Provider store = { store } >
+    <
+    App / >
+    <
+    /Provider> <
+    /HashRouter> <
+    /React.StrictMode>,
+    document.getElementById('root')
+);
 
 
 
