beb654591d1efde1e2fd758f55c2d75e133d6101
KolesnikAntony <50579516+KolesnikAntony@users.noreply.github.com>
2021-01-12 20:39:26 +0200

    delete test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, {useEffect} from 'react';
 import style from './App.module.css';
 import Sidebar from './components/Sidebar/Sidebar';
-import {Redirect, Route, Switch} from "react-router-dom";
+import {HashRouter, Redirect, Route, Switch, withRouter} from "react-router-dom";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import Header from "./components/Header/Header";
 import Login from "./components/Login/login";
-import {connect} from "react-redux";
+import {connect, Provider} from "react-redux";
 import {initializing} from "./redux/app-reducer";
 import Preloader from "./common/Preloader/Preloader";
 import Error404 from "./components/ErrorComponents/Error404";
 import {requestInitialized} from "./redux/selectors";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {compose} from "redux";
+import store from "./redux/redux-store";
 
 
 const App = ({initializing,initialized}) => {
@@ -45,4 +47,17 @@ const mapStateToProps = state => (
     }
 );
 
-export default connect(mapStateToProps, {initializing} )(App);
+const AppContainer = compose(withRouter,
+    connect(mapStateToProps, {initializing} ))(App);
+
+const AppMain = () => {
+    return  <HashRouter>
+        <Provider store={store}>
+            <AppContainer/>
+        </Provider>
+    </HashRouter>
+};
+
+
+export default AppMain;
+
--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {HashRouter} from "react-router-dom";
-import store from "./redux/redux-store";
-import {Provider} from "react-redux";
+import AppMain from "./App";
 
 
 ReactDOM.render(<React.StrictMode>
-        <HashRouter>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </HashRouter>
+        <AppMain/>
     </React.StrictMode>,
     document.getElementById('root')
 );
+
 
 
 // If you want to start measuring performance in your app, pass a function
